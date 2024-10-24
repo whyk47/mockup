f3ca828567afa5f657b9f4910a2f24c20d085fb8
Yen Kit Chow <chowyenkit@gmail.com>
2024-10-24 17:44:30 +0800

    '.'

--- recruitment/urls.py
+++ recruitment/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "recruitment"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("details/<int:job_id>", views.details, name="details"),
 ]
--- recruitment/views.py
+++ recruitment/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
 
+from .models import *
+
 
 def index(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
     return render(request, 'recruitment/index.html', {
-        
+        'jobs': Job.objects.all(),
     })
+
+def details(request: HttpRequest, job_id: int) -> HttpResponse | HttpResponseRedirect:
+    return HttpResponse(f'Job {job_id}')
