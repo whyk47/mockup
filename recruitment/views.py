103f9dc50a4f3de43278b36921b4d7dfa3176800
Yen Kit Chow <chowyenkit@gmail.com>
2024-10-27 11:31:42 +0800

    '.'

--- recruitment/views.py
+++ recruitment/views.py
@@ -5,13 +5,16 @@ from .models import *
 
 
 def index(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
-    queryset = Job.objects
+    queryset = Job.objects.all()
+    if request.GET.get('query'):
+        queryset = queryset.filter(title__icontains=request.GET.get('query'))
     if request.GET.get('min_salary'):
         queryset = queryset.filter(monthly_pay__gte=request.GET.get('min_salary'))
     if request.GET.get('max_salary'):
         queryset = queryset.filter(monthly_pay__lte=request.GET.get('max_salary'))
     return render(request, 'recruitment/index.html', {
         'jobs': queryset,
+        **request.GET.dict(),
     })
 
 def details(request: HttpRequest, job_id: int) -> HttpResponse | HttpResponseRedirect:
