8c17a9f879f0b1c91d965be56a5621d1005009ea
Yen Kit Chow <chowyenkit@gmail.com>
2024-11-18 10:30:14 +0800

    '.'

--- push.bat
+++ push.bat
@@ -1,3 +1,7 @@
 git add .
 git commit -m '.'
 git push
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 980921716072.dkr.ecr.ap-southeast-1.amazonaws.com
+docker build -t yk47/mockup .
+docker tag yk47/mockup:latest 980921716072.dkr.ecr.ap-southeast-1.amazonaws.com/yk47/mockup:latest
+docker push 980921716072.dkr.ecr.ap-southeast-1.amazonaws.com/yk47/mockup:latest
