d0a06c3a6b4af5fd315bfd81afd0dd74593d224a
Chow Yen Kit <chowyenkit@gmail.com>
2024-11-20 16:44:48 +0800

    .

--- recruitment/views.py
+++ recruitment/views.py
@@ -20,6 +20,7 @@ def details(request: HttpRequest, job_id: int) -> HttpResponse | HttpResponseRed
     })
 
 def search(request: HttpRequest) -> HttpResponse | HttpResponseRedirect:
+    # TODO: Pagination
     # page = get_page(request, job.filter(request).pop('jobs'))
     return render(request, 'recruitment/search.html', {
         # 'page': page,
